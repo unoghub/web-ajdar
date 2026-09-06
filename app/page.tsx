bb9738274728e156d174ce8282a1914e553e6352
emirsassan <endwse@gmail.com>
2026-09-06 11:07:23 +0300

    feat: about us section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { AboutSection } from "@/components/about";
 import { HeroSection } from "@/components/hero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
       <div className="bg-[linear-gradient(100.29deg,rgba(137,255,150,0.06)_0.21%,rgba(139,189,255,0.06)_45.91%,rgba(173,148,255,0.06)_87.43%)]">
         <HeroSection />
       </div>
+      <AboutSection />
     </div>
   );
 }
